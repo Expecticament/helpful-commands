100ba1fb75912aa8a6c73e1399b0f7df803718c4
expecticament <contact@expecticament.com>
2026-07-19 01:50:31 +0300

    Set folder name to 'helpful_commands4' to avoid losing all data when updating from older mod version.

--- common/src/main/java/com/expecticament/helpfulcommands/Constants.java
+++ common/src/main/java/com/expecticament/helpfulcommands/Constants.java
@@ -7,7 +7,7 @@ public class Constants {
     public static final String MOD_ID = "helpfulcommands";
     public static final String MOD_SHORT_ID = "hc";
     public static final String MOD_NAME = "Helpful Commands";
-    public static final String FOLDER_NAME = MOD_ID + "4";
+    public static final String FOLDER_NAME = "helpful_commands4";
 
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_NAME);
 }
